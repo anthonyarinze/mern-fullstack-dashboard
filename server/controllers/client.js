ca7d12abfa9cab7f792dc715243b17865d045dda
Anthony Arinze <anthonyarinze28@gmail.com>
2023-03-05 21:30:28 +0100

    Final Product

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -87,7 +87,7 @@ export const getGeography = async (req, res) => {
     }, {});
 
     const formattedLocations = Object.entries(mappedLocations).map(
-      ({ country, count }) => {
+      ([country, count]) => {
         return { id: country, value: count };
       }
     );
